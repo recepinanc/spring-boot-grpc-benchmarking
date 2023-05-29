6712bd4ebc0d9659573be8497df8f55ae0f52a0a
kkty39 <yingyitong39@gmail.com>
2023-05-29 15:47:57 -0700

    added new module for grpc-rest server

---
+++ sample-grpc-springboot-server/src/main/java/com/kkty39/samplegrpcrestserver/SampleGRPCRESTApplication.java
@@ -0,0 +1,14 @@
+package com.kkty39.samplegrpcrestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGRPCRESTApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SampleGRPCRESTApplication.class, args);
+    }
+
+}
